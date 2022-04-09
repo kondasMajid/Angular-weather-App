56579f786a386a5ad962139e644133e5e947e132
konasMajid <kondasmajid@Gmail.com>
2022-04-09 14:10:52 +0000

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {FormControl, Validators} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+import { FormControl, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import * as moment from 'moment';
 
 @Component({
@@ -10,41 +10,41 @@ import * as moment from 'moment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Data ; 
+  Data;
   now = moment().format('LLLL');  // moment Js
-  day =  moment().format('ddd, LT');
-  userInput:string = '';
-  message ;
- 
+  day = moment().format('ddd, LT');
+  userInput: string = '';
+  message;
 
-  constructor(private http: HttpClient) {  }
 
- days(){
-  moment().format('ddd, LT');
-  setInterval(this.days, 1000)
-    
-  }  
-  
-    //Get weather function
-      getWeather (){
-        if(this.userInput == null){
-          alert('Enter City name')
-        
-        }
-        else{
-      let weatherAPi = 'http://api.openweathermap.org/data/2.5/weather?q='+this.userInput+'&appid=6cb36b639ee172ba6bc892bec0636b76&units=metric';
-        console.log(this.userInput)
+  constructor(private http: HttpClient) { }
 
-        this.Data = this.http.get(weatherAPi).subscribe( data =>  {
+  days() {
+    moment().format('ddd, LT');
+    setInterval(this.days, 1000)
+
+  }
+
+  //Get weather function
+  getWeather() {
+    if (this.userInput == null) {
+      alert('Enter City name')
+
+    }
+    else {
+      let weatherAPi = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.userInput + '&APPID=d37a62076c5d10716ae0a27b849bc36c';
+      // &units=metric
+      console.log(this.userInput)
+
+      this.Data = this.http.get(weatherAPi).subscribe(data => {
         this.Data = data;
         console.log(this.Data);
       });
-      
-        }
-      }
+    }
+  }
 
-   ngOnInit() {
-    this.getWeather();
-   }
+  ngOnInit() {
+    // this.getWeather();
+  }
 
 }
